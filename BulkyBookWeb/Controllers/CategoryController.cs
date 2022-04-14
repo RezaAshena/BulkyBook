4c9e06f1acbe2d196785ae8b9e941013f7f2e9eb
Reza Ashena <Ashena.reza@gmail.com>
2022-04-14 09:16:50 -0400

    Create Category

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -23,5 +23,15 @@ namespace BulkyBookWeb.Controllers
         {
             return View();
         }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
