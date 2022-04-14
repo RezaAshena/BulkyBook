e4af403f809d740ab05b12b1a382f4a797a7677d
Reza Ashena <Ashena.reza@gmail.com>
2022-04-14 09:09:23 -0400

    Create Category View

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -17,5 +17,11 @@ namespace BulkyBookWeb.Controllers
             IEnumerable<Category> objCategoryList=_db.Categories;
             return View(objCategoryList);
         }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
